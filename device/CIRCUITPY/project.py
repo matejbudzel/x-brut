7f36389ac71ba3f62380180a675a60699cf92e8d
Matej Budzel <budzel@gmail.com>
2026-09-16 08:45:03 +0200

    Add xViewer project paths

---
+++ device/CIRCUITPY/project.py
@@ -0,0 +1,8 @@
+"""xViewer project entrypoint.
+
+Project state is deliberately isolated from the reusable X Brut base.
+"""
+PROJECT_NAME = "xViewer"
+PROJECT_VERSION = "0.1.0"
+PROJECT_CONFIG_PATH = "/project-conf.json"
+DOCUMENTS_DIRECTORY = "/xviewer-docs"
---
+++ xbrut/xviewer.py
@@ -0,0 +1,3 @@
+"""Desktop counterparts of xViewer's device-root paths."""
+PROJECT_CONFIG_NAME = "project-conf.json"
+DOCUMENTS_DIRECTORY = "xviewer-docs"
